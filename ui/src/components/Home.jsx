015e25335241b6b4f1f4308547d46a4e7ceaf366
souravcsewala <souravssc077@gmail.com>
2025-08-17 06:09:38 +0530

    add

--- ui/src/components/Home.jsx
+++ ui/src/components/Home.jsx
@@ -99,10 +99,10 @@ function Home() {
         <div className="flex h-screen items-center justify-center">
           <div className="border-2 border-dashed px-4 py-2 md:px-8 md:py-6 border-indigo-400 rounded-lg shadow-lg">
             <h1 className="text-3xl font-bold text-center mb-4">
-              Convert Word to PDF Online
+              Convert DOCX to PDF Online
             </h1>
             <p className="text-sm text-center mb-5">
-              Easily convert Word documents to PDF format online, without having
+              Easily convert DOCX documents to PDF format online, without having
               to install any software.
             </p>
 
